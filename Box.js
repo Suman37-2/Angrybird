cee569199a7dfd868b2e442d44dc4cb7fa39c905
Suman37-2 <72697297+Suman37-2@users.noreply.github.com>
2021-01-24 18:55:16 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,19 +1,8 @@
-class Box{
-    constructor(x,y,width,height){
-
-        var op={
-            restitution:1
-        }
-        this.body=Bodies.rectangle(x,y,width,height,op);
-        this.width=width;
-        this.height=height;
-        World.add(myWorld,this.body);   
-    }
-     
-    display(){
-        var pos=this.body.position;
-        fill("blue");
-        rectMode(CENTER);
-        rect(pos.x,pos.y,this.width,this.height);
+class Box extends Baseclass {
+    constructor(x,y){
+    super(x,y,70,70);
+    this.image=loadImage("sprites/wood1.png");
     }
 }
+
+
